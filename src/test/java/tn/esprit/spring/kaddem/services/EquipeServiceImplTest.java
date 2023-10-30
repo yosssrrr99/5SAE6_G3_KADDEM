6f781eb5ec71f6130da605d0cad4241d7531eee7
houssemjr <houssem.jrad@esprit.tn>
2023-10-30 13:18:54 +0100

    third

--- src/test/java/tn/esprit/spring/kaddem/services/EquipeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/EquipeServiceImplTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.spring.kaddem.entities.Equipe;
+import tn.esprit.spring.kaddem.entities.Niveau;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -30,6 +31,36 @@ public class EquipeServiceImplTest {
 
         Assert.assertFalse(equipes.isEmpty());
     }
+
+    @Test
+    public void testAddEquipe() {
+        Equipe equipe = new Equipe();
+        equipe.setNomEquipe("testequipe");
+
+
+        Equipe equipeAjoutee = equipeService.addEquipe(equipe);
+
+       Assert.assertNotNull(equipeAjoutee);
+
+        Assert.assertNotNull(equipeAjoutee.getIdEquipe());
+
+
+
+    }
+    @Transactional
+    @Test
+   public void testUpdateEquipe() {
+        // Récupérez un étudiant existant
+        Equipe existingEquipe= equipeService.retrieveEquipe(2);
+
+        // Modifiez les données de l'étudiant
+        existingEquipe.setNiveau(Niveau.SENIOR);
+        Equipe updatedEquipe = equipeService.updateEquipe(existingEquipe);
+
+        // Vérifiez que l'étudiant a été correctement mis à jour
+        Assert.assertEquals(Niveau.SENIOR, updatedEquipe.getNiveau());
+    }
+
 
 
 }
